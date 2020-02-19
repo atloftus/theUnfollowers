880b91b15a0d2587c38561cc8041899cc5c65d4b
Alex Loftus <atloftus@wisc.edu>
2020-02-19 17:07:45 -0600

    Got the web Driver to work

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
 
 namespace theUnfollowers
 {
@@ -6,6 +13,11 @@ namespace theUnfollowers
     {
         static void Main(string[] args)
         {
+            string ChromeDriverRelativePath = (Directory.GetCurrentDirectory().Split(new string[] { "repos" }, StringSplitOptions.None))[0] + @"repos\Hunters\chromedriver_win32";
+            ChromeDriver Driver = new ChromeDriver(ChromeDriverRelativePath);
+            string url = "https://www.google.com/";
+            Driver.Navigate().GoToUrl(url);
+            Thread.Sleep(2000);
             Console.WriteLine("Hello World! Lets see if this works...");
         }
     }
